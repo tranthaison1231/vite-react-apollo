ca67af1a726cf21af09e30359d28e70bf3b99248
Son Tran <son.tran@team.enouvo.com>
2022-10-27 09:52:13 +0700

    feat(master): upgrade package

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Loading } from '@enouvo/react-uikit';
 import { Suspense } from 'react';
 import { useRoutes } from 'react-router-dom';
-// eslint-disable-next-line import/no-unresolved
 import routes from '~react-pages';
 
 function App() {
